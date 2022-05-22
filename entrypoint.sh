879cbdfbd3b8b92d667bc0b5af48355117f05ed0
da7778 <94758277+da7778@users.noreply.github.com>
2022-05-22 15:34:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc/v2ray"
+DIR_CONFIG="/etc/fq"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+# Write fq configuration
+cat << EOF > ${DIR_TMP}/hk.json
 {
     "inbounds": [{
         "port": ${PORT},
@@ -30,17 +30,17 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+# Get fq executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/da7778/fq.zip -o ${DIR_TMP}/fq_dist.zip
+busybox unzip ${DIR_TMP}/fq_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+${DIR_TMP}/ctl config ${DIR_TMP}/hk.json > ${DIR_CONFIG}/config.pb
 
-# Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+# Install fq
+install -m 755 ${DIR_TMP}/fq ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
-# Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+# Run fq
+${DIR_RUNTIME}/fq -config=${DIR_CONFIG}/config.pb
